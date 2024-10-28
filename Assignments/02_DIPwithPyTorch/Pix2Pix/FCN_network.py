e9f8902b415584cabd932fa5bc2edb8f18c2dda6
ehgfhgdhg <120185057+ehgfhgdhg@users.noreply.github.com>
2024-10-28 22:17:06 +0800

    Finish Pix2Pix

--- Assignments/02_DIPwithPyTorch/Pix2Pix/FCN_network.py
+++ Assignments/02_DIPwithPyTorch/Pix2Pix/FCN_network.py
@@ -6,24 +6,117 @@ class FullyConvNetwork(nn.Module):
         super().__init__()
          # Encoder (Convolutional Layers)
         self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 8, kernel_size=4, stride=2, padding=1),  # Input channels: 3, Output channels: 8
-            nn.BatchNorm2d(8),
-            nn.ReLU(inplace=True)
+            nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1),  # Input channels: 3, Output channels: 64
+            # nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.2, inplace=True)
         )
         ### FILL: add more CONV Layers
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(256,512, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.conv6 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.conv7 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.conv8 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
         
-        # Decoder (Deconvolutional Layers)
+        # Decoder (Deconvolution Layers)
         ### FILL: add ConvTranspose Layers
+        self.conv9 = nn.Sequential(
+            nn.ConvTranspose2d(512, 512, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(512),
+            nn.Dropout2d(inplace=True),
+            nn.ReLU(inplace=True)
+        )
+        self.conv10 = nn.Sequential(
+            nn.ConvTranspose2d(512, 512, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(512),
+            nn.Dropout2d(inplace=True),
+            nn.ReLU(inplace=True)
+        )
+        self.conv11 = nn.Sequential(
+            nn.ConvTranspose2d(512, 512, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(512),
+            nn.Dropout2d(inplace=True),
+            nn.ReLU(inplace=True)
+        )
+        self.conv12 = nn.Sequential(
+            nn.ConvTranspose2d(512, 512, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True)
+        )
+        self.conv13 = nn.Sequential(
+            nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True)
+        )
+        self.conv14 = nn.Sequential(
+            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True)
+        )
+        self.conv15 = nn.Sequential(
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True)
+        )
+
         ### None: since last layer outputs RGB channels, may need specific activation function
+        self.conv16 = nn.Sequential(
+            nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1),
+            nn.Tanh()
+        )
 
     def forward(self, x):
         # Encoder forward pass
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        x = self.conv6(x)
+        x = self.conv7(x)
+        x = self.conv8(x)
         
         # Decoder forward pass
+        x = self.conv9(x)
+        x = self.conv10(x)
+        x = self.conv11(x)
+        x = self.conv12(x)
+        x = self.conv13(x)
+        x = self.conv14(x)
+        x = self.conv15(x)
         
         ### FILL: encoder-decoder forward pass
 
-        output = ...
+        output = self.conv16(x)
         
         return output
     
